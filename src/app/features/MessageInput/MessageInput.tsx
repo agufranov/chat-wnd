27efa25a8172436c066e379ccec9bd1da849574d
agufranov <al.gufranov@gmail.com>
2025-12-23 20:45:11 +0300

    styling for MessageInput

--- src/app/features/MessageInput/MessageInput.tsx
+++ src/app/features/MessageInput/MessageInput.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState, type FormEvent } from "react";
 import type React from "react";
 import style from "./MessageInput.module.css";
 import Send from "./assets/send.svg?react";
+import cn from "classnames";
 
 type MessageInputProps = {
   onSubmit: (message: string) => void;
@@ -9,7 +10,7 @@ type MessageInputProps = {
 
 export const MessageInput: React.FC<MessageInputProps> = ({ onSubmit }) => {
   const [inputState, setInputState] = useState("");
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
     inputRef.current?.focus();
@@ -21,20 +22,34 @@ export const MessageInput: React.FC<MessageInputProps> = ({ onSubmit }) => {
     onSubmit(inputState.trim());
   };
 
+  const adjustHeight = () => {
+    const textarea = inputRef.current;
+    if (!textarea) return;
+
+    textarea.style.height = "auto"; // Сброс
+    textarea.style.height = `${textarea.scrollHeight + 1}px`;
+  };
+
+  const handleInput = (e: FormEvent<HTMLTextAreaElement>) => {
+    setInputState((e.currentTarget as HTMLTextAreaElement).value);
+    adjustHeight();
+  };
+
   return (
-    <form className={style.root} onSubmit={handleSubmit}>
-      <input
-        className={style.input}
+    <form
+      className={cn([style.root, style.messageInputContainer])}
+      onSubmit={handleSubmit}
+    >
+      <textarea
+        rows={1}
+        className={style.messageInput}
         value={inputState}
         placeholder="Введите сообщение..."
-        onInput={(e) =>
-          setInputState((e.currentTarget as HTMLInputElement).value)
-        }
+        onInput={handleInput}
         ref={inputRef}
       />
-      <button type="submit" disabled={!inputState}>
+      <button className={style.sendButton} type="submit" disabled={!inputState}>
         <Send width={24} height={24} />
-        Отправить
       </button>
     </form>
   );
