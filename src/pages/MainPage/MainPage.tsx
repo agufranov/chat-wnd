2b9660de35c27799a90224140b365e9f8a9b17b8
agufranov <al.gufranov@gmail.com>
2025-12-24 19:04:15 +0300

    add global key handler

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,22 @@
 import type { Chat } from "@/shared/types";
 import { ChatList } from "@/widgets/ChatList/ChatList";
 import { ChatWindow } from "@/widgets/ChatWindow/ChatWindow";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "./MainPage.module.css";
 
 export const MainPage: React.FC = () => {
   const [selectedChat, setSelectedChat] = useState<Chat | null>(null);
+
+  useEffect(() => {
+    const keyHandler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") setSelectedChat(null);
+    };
+
+    document.addEventListener("keydown", keyHandler);
+
+    return () => document.removeEventListener("keydown", keyHandler);
+  });
+
   return (
     <main className={style.main}>
       <aside className={style.sidebar}>
